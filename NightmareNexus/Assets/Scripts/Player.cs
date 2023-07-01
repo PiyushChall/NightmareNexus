33c14c84a5c95cec6836adf0e97ad9f1795a0d9d
PiyushChall <115055657+PiyushChall@users.noreply.github.com>
2023-07-01 23:36:23 +0530

    Added Player movements

--- NightmareNexus/Assets/Scripts/Player.cs
+++ NightmareNexus/Assets/Scripts/Player.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float movespeed;
-    public Rigidbody2D rb;
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 20f;
+    private bool isFacingRight = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        float horiz = Input.GetAxisRaw("Horizontal");
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
     }
+
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2();
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
